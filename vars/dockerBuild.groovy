9e70d89e98abf473c1ec84f27d0610c72f01b472
manjularajamani <100277950+manjularajamani@users.noreply.github.com>
2024-05-27 15:34:19 +0530

    Add Docker build functions

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,6 +1,9 @@
-def call() {
-        sh """
-            docker login --username="${username}" --password="${password}"
-        """
-    }
+def call(String project, String imageTag, String hubUser, String password){
+    
+    sh """
+     docker login --username="${hubUser}" --password="${password}"
+     docker image build -t ${hubUser}/${project} . 
+     docker image tag ${hubUser}/${project} ${hubUser}/${project}:${imageTag}
+     docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
+    """
 }
