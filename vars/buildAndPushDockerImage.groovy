c5ae22f1355bb5ec8d74e7339402ef777056aa60
manjularajamani <100277950+manjularajamani@users.noreply.github.com>
2024-05-27 17:23:33 +0530

    Updated build functions

--- vars/buildAndPushDockerImage.groovy
+++ vars/buildAndPushDockerImage.groovy
@@ -2,7 +2,8 @@ def call(String aws_account_id, String region, String ecr_repoName, String tagNa
     
     sh """
 
-     aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com     docker build -t ${ecr_repoName} .
+     aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com  
+     docker build -t ${ecr_repoName} .
      docker tag ${ecr_repoName}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:${tagName}
      docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:${tagName}
     """
