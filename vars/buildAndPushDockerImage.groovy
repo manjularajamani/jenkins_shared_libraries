1ee6ce082209a7919c2d6a04041a055487c9d22e
manjularajamani <100277950+manjularajamani@users.noreply.github.com>
2024-05-27 21:16:51 +0530

    Updated build functions

--- vars/buildAndPushDockerImage.groovy
+++ vars/buildAndPushDockerImage.groovy
@@ -1,7 +1,16 @@
-def dockerBuildAndPush(){
+// def dockerBuildAndPush(){
+//     sh 'rm -f ~/.dockercfg ~/.docker/config.json || true'
+//         withDockerRegistry(credentialsId: 'ecr:us-east-2:aws', url: 'https://938508880305.dkr.ecr.us-east-2.amazonaws.com') {
+//              def customImage = docker.build("node-app:${env.BUILD_ID}")
+//              customImage.push()
+// }    
+// }
+
+
+def call() {
     sh 'rm -f ~/.dockercfg ~/.docker/config.json || true'
-        withDockerRegistry(credentialsId: 'ecr:us-east-2:aws', url: 'https://938508880305.dkr.ecr.us-east-2.amazonaws.com') {
-             def customImage = docker.build("node-app:${env.BUILD_ID}")
-             customImage.push()
-}    
+    withDockerRegistry(credentialsId: 'ecr:us-east-2:aws', url: 'https://938508880305.dkr.ecr.us-east-2.amazonaws.com') {
+        def customImage = docker.build("node-app:${env.BUILD_ID}")
+        customImage.push()
+    }
 }
