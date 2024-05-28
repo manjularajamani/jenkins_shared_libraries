c890c3f309b478dd3c2aabe5bf19f5ed6493c533
manjularajamani <100277950+manjularajamani@users.noreply.github.com>
2024-05-28 12:51:06 +0530

    Updated build

--- vars/dockerBuildAndPush.groovy
+++ vars/dockerBuildAndPush.groovy
@@ -1,4 +1,4 @@
-def call() {
+def BuildAndPush() {
     sh 'rm -f ~/.dockercfg ~/.docker/config.json || true'
     withDockerRegistry(credentialsId: 'ecr:us-east-2:aws', url: 'https://938508880305.dkr.ecr.us-east-2.amazonaws.com') {
         def customImage = docker.build("node-app:${env.BUILD_ID}")
