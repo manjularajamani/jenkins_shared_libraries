264c969d5f1827a7b3feb4e0a1578765fb2c30f0
manjularajamani <100277950+manjularajamani@users.noreply.github.com>
2024-05-30 15:16:35 +0530

    Updated ECS library

--- vars/ecsDeploy.groovy
+++ vars/ecsDeploy.groovy
@@ -14,7 +14,7 @@ def clonerepo(String repoUrl, String branch = 'master') {
 def buildandpush(String registryUrl, String credentialId) {
     sh 'rm -f ~/.dockercfg ~/.docker/config.json || true'
     docker.withRegistry(registryUrl, credentialId) {
-        def customImage = docker.build("sit-ui-dev:${env.BUILD_ID}")
+        def customImage = docker.build("testing:${env.BUILD_ID}")
         customImage.push()
     }
 }
