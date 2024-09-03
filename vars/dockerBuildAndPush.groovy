14b8a292d9e3a71c17957882e6823578c41261c3
Manjula Rajamani <100277950+manjularajamani@users.noreply.github.com>
2024-09-03 13:14:07 +0530

    Update dockerBuildAndPush.groovy

--- vars/dockerBuildAndPush.groovy
+++ vars/dockerBuildAndPush.groovy
@@ -1,15 +1,4 @@
-// def BuildAndPush(String registryUrl, String credentialId) {
-//     sh 'rm -f ~/.dockercfg ~/.docker/config.json || true'
-//     docker.withRegistry(registryUrl, credentialId) {
-//         def customImage = docker.build("sit-ui-dev:${env.BUILD_ID}")
-//         customImage.push()
-//     }
-// }
-
-
-
-// In dockerBuildAndPush.groovy (shared library)
-def call(String registryUrl, String credentialId) {
+def BuildAndPush(String registryUrl, String credentialId) {
     sh 'rm -f ~/.dockercfg ~/.docker/config.json || true'
     docker.withRegistry(registryUrl, credentialId) {
         def customImage = docker.build("sit-ui-dev:${env.BUILD_ID}")
